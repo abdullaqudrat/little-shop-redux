5b4037ccff3c0177a21e4649c4a242b4cbd3bf9c
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-08-16 16:15:18 -0600

    ATP for merchant create form, fillable with merchant name

--- spec/features/merchant_create_spec.rb
+++ spec/features/merchant_create_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe 'Merchant create page' do
     visit "/merchants/new"
     expect(page).to have_content('Create New Merchant')
   end
+
+  it 'should fill in form with Merchant name' do
+    visit "/merchants/new"
+    fill_in('merchant[name]', with: "Abdulla's Shop")
+  end
 end
