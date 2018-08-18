43ee489d7cd4411cb42859dd472e849132d81162
Tara Craig <tara.craig7@gmail.com>
2018-08-18 11:40:49 -0600

    Adds merchant csv parser to seeds, updates merchants data table with timestamps for created at and updated at.

--- db/migrate/20180816013511_create_merchants.rb
+++ db/migrate/20180816013511_create_merchants.rb
@@ -1,7 +1,8 @@
 class CreateMerchants < ActiveRecord::Migration[5.2]
   def change
     create_table :merchants do |m|
-      m.text  :name
+      m.text        :name
+      m.timestamps  null: false
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,8 @@ ActiveRecord::Schema.define(version: 2018_08_16_013511) do
 
   create_table "merchants", force: :cascade do |t|
     t.text "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
