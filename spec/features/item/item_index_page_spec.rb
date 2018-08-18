9082a814afe1a713179073dde91274064b750d52
abdyqew <aqudrat630@gmail,com>
2018-08-18 16:05:03 -0600

    WIP tests item index shell

--- spec/features/item/item_index_page_spec.rb
+++ spec/features/item/item_index_page_spec.rb
@@ -3,6 +3,11 @@ RSpec.describe 'Item index page' do
     @item_1 = Item.create(name: "Turing Ale", description: "Beer", unit_price: 4, image: 'pic_of_beer_here')
     @item_2 = Item.create(name: "Delerium Tremens", description: "Beer", unit_price: 12, image: 'pic_of_beer_here')
   end
+  it 'should item heading' do
+    visit '/items'
+    expect(page).to have_content("Items")
+  end
+
   it 'should show all item names' do
     visit '/items'
     expect(page).to have_content(@item_1.name)
@@ -33,24 +38,4 @@ RSpec.describe 'Item index page' do
     expect(current_path).to eq("/items/new")
   end
 
-  xit 'item name should have link to item edit page' do
-    visit '/items'
-    within("#item-#{@item_1.id}-edit") do
-      click_on "Edit"
-    end
-    expect(current_path).to eq("/items/#{@item_1.id}/edit")
-  end
-
-  xit 'should not show deleted item at index after Delete button clicked' do
-
-    visit "/items"
-
-    expect(page).to have_content("Beer World")
-
-    within("#item-#{@item_1.id}-delete") do
-      click_on "Delete"
-    end
-
-    expect(page).to_not have_content("Beer World")
-  end
 end
