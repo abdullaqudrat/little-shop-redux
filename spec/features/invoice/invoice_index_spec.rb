325fb44e87a5ae6b5870138ed01b4618b20d1fb9
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-08-19 17:27:05 -0600

    WIP index id tags not being identified, only has spec for index

--- spec/features/invoice/invoice_index_spec.rb
+++ spec/features/invoice/invoice_index_spec.rb
@@ -22,4 +22,25 @@ RSpec.describe 'Invoice index page' do
     expect(current_path).to eq("/invoices/#{@invoice_1.id}")
   end
 
+  it 'invoice should have link to invoice edit page' do
+    visit '/invoices'
+    save_and_open_page
+
+    within("#invoice-#{@invoice_1.id}-edit") do
+      click_on "Edit"
+    end
+    expect(current_path).to eq("/invoices/#{@invoice_1.id}/edit")
+  end
+
+  it 'should not show deleted invoice at index after Delete button clicked' do
+    visit "/merchants"
+    expect(page).to have_content("77473839")
+
+    within("#invoice-#{@invoice_1.id}-delete") do
+      click_on "Delete"
+    end
+
+    expect(page).to_not have_content("77473839")
+  end
+
 end
