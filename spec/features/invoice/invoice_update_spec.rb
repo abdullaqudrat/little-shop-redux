dac5de5bf955181039b096aae0976c31c9969e92
Tara Craig <tara.craig7@gmail.com>
2018-08-19 19:04:21 -0600

    Add edit view page for invoices and started implementation. The first two tests passed until I tried to make a dropdown menu and now there are 5 failures. Sorry.

--- spec/features/invoice/invoice_update_spec.rb
+++ spec/features/invoice/invoice_update_spec.rb
@@ -1,47 +1,48 @@
 RSpec.describe 'Invoice update page' do
   before(:each) do
-    @invoice = Invoice.create(merchant_id: 1, status: 'Pending')
+    @invoice_1 = Invoice.create(merchant_id: 1, status: 'Pending')
+    @invoice_2 = Invoice.create(merchant_id: 1, status: 'Returned')
     @merchant_1 = Merchant.create(name: "Wine World")
   end
 
   it 'should show invoice id' do
-    visit "/invoices/#{@invoice.id}/edit"
-
-    expect(page).to have_content("Invoice: #{@invoice.id}")
+    visit "/invoices/#{@invoice_1.id}/edit"
+save_and_open_page
+    expect(page).to have_content("Invoice: #{@invoice_1.id}")
   end
 
-  xit 'should show invoice status' do
-    visit "/invoices/#{@invoice.id}/edit"
+  it 'should show invoice status' do
+    visit "/invoices/#{@invoice_1.id}/edit"
 
-    expect(page).to have_content("#{@invoice.status}")
+    expect(page).to have_content("#{@invoice_1.status}")
   end
 
   xit 'should select status from drop down menu' do
-    visit "/invoices/#{@invoice.id}/edit"
+    visit "/invoices/#{@invoice_1.id}/edit"
 
     select("Returned", from: "status_dropdown")
   end
 
   xit 'cancels form when user clicks cancel button and goes to show page' do
-    visit "/invoices/#{@invoice.id}/edit"
+    visit "/invoices/#{@invoice_1.id}/edit"
     click_on 'Cancel'
 
-    expect(current_path).to eq("/invoices/#{@invoice.id}")
+    expect(current_path).to eq("/invoices/#{@invoice_1.id}")
   end
 
-  xit 'should link to item show after Update button clicked' do
-    visit "/invoices/#{@invoices.id}/edit"
+  xit 'should link to invoice show after Update button clicked' do
+    visit "/invoices/#{@invoice_1.id}/edit"
 
     click_on "Update Invoice"
-    expect(current_path).to eq("/invoices/#{@invoices.id}")
+    expect(current_path).to eq("/invoices/#{@invoice_1.id}")
   end
 
-  xit 'should show updated item at show page after Update button clicked' do
+  xit 'should show updated invoice at show page after Update button clicked' do
 
-    visit "/invoices/#{@invoice.id}/edit"
+    visit "/invoices/#{@invoice_1.id}/edit"
 
     select("Returned", from: "status_dropdown")
-    click_on "Update Item"
+    click_on "Update Invoice"
 
     expect(page).to have_content("Returned")
   end
