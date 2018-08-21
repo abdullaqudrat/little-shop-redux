bc4f43cf28e6bdf2ff8525255606024fadbe7cc5
Tara Craig <tara.craig7@gmail.com>
2018-08-21 10:20:38 -0600

    Spacing edits.

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -66,19 +66,16 @@ RSpec.describe Invoice do
 
     it 'is valid with attributes' do
       invoice = Invoice.create(merchant_id: 12345, status: "pending")
-
       expect(invoice).to be_valid
     end
 
     it 'is invalid without a merchant_id' do
       invoice = Invoice.create(status: "pending")
-
       expect(invoice).to_not be_valid
     end
 
     it 'is invalid without a status' do
       invoice = Invoice.create(merchant_id: 12345)
-
       expect(invoice).to_not be_valid
     end
   end
